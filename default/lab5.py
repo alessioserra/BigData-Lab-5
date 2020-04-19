67aa65701f51dc2809875a3d568c6e214d47247d
zierp <zierp@LAPTOP-4TV42EQM>
2020-04-19 16:58:46 +0200

    Task 1 completed

--- default/lab5.py
+++ default/lab5.py
@@ -1,5 +1,27 @@
-'''
-Created on 19 apr 2020
+import sys
+from pyspark import SparkConf, SparkContext
 
-@author: zierp
-'''
+#Initialize Spark application
+conf = SparkConf().setAppName("Lab_5")
+sc = SparkContext(conf = conf)
+
+#Input/Output folder and prefix string(the values are specified by means of three input parameters)
+inputPath  = sys.argv[1] 
+outputPath = sys.argv[2] 
+prefix = sys.argv[3] 
+
+'TASK 1'
+#Read data
+inputFile = sc.textFile(inputPath)
+#Filter word that start with chosen prefix
+RDDwordFreq = inputFile.filter(lambda line: line.startwith(prefix))
+
+#Count the number of filtered lines and max frequency
+numberLines = RDDwordFreq.count()
+maxFreq = RDDwordFreq.reduce(lambda e1,e2 : max(e1,e2))
+
+print("Number of Lines = "+str(numberLines)+" & max_freq = "+str(maxFreq))
+
+#Save the results in the output path
+RDDResult = sc.parallelize([("Number of Lines = "+str(numberLines)+" & max_freq = "+str(maxFreq))])
+RDDResult.saveAsTextFile(outputPath)
