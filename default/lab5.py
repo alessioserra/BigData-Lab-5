85df307e4f7eac0963f75dca2e98d66bf87c4c1e
zierp <zierp@LAPTOP-4TV42EQM>
2020-04-19 17:54:26 +0200

    Lab5 completed

--- default/lab5.py
+++ default/lab5.py
@@ -16,9 +16,12 @@ inputFile = sc.textFile(inputPath)
 #Filter word that start with chosen prefix
 RDDwordFreq = inputFile.filter(lambda line: line.startswith(prefix))
 
-#Count the number of filtered lines and max frequency
+#Count the number of filtered lines
 numberLines = RDDwordFreq.count()
-maxFreq = RDDwordFreq.reduce(lambda e1,e2 : max(e1,e2))
+
+#Get only the frequencies and choose the max
+maxfreqRDD = RDDwordFreq.map(lambda line: float(line.split("\t")[1]))
+maxFreq = maxfreqRDD.reduce(lambda e1,e2 : max(e1,e2))
 
 print("Number of Lines = "+str(numberLines)+" & max_freq = "+str(maxFreq))
 
@@ -28,22 +31,15 @@ RDDResult.saveAsTextFile(outputPath)
 
 'TASK 2'
 #Keep only the lines with a frequency freq greater than 0.8*maxfreq
-#Define function for filter
-def newFilter(line):
-    freq = (line.split('\t'))[1]
-    print(freq)
-    if ( int(freq) > (int(maxFreq))*0.8):
-        return True
-    else: return False
-    
-RDDwordsNew = RDDwordFreq.filter(newFilter)
+RDDwordsNew = RDDwordFreq.filter(lambda line: float(line.split("\t")[1]) > 0.8*maxFreq)
 #Count the number of filtered lines and print in standard output
 numberLines2 = RDDwordsNew.count()
 print("Number of filtered lines = "+str(numberLines2))
 
 #Save the selected words (without frequency) in an output folder (one word per line)
 RDDBestWords = RDDwordsNew.map(lambda line : line.split("\t")[0])
-#Save the results in the output path
-RDDBestWords.saveAsTextFile(outputPath)
+#Save the results in a new output path
+outputPath2 = sys.argv[4] 
+RDDBestWords.saveAsTextFile(outputPath2)
 
 sc.stop()
